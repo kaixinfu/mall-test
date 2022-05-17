4f3080618a97314fafe6116b86995591b42e7b16
liu kaixin <1360517661@qq.com>
2022-05-17 13:16:56 +0800

    add product.detail

--- src/models/connect.d.ts
+++ src/models/connect.d.ts
@@ -1,14 +1,17 @@
 // model state 类型
 import { UserModelState } from './user';
 import { Location, Dispatch } from 'umi';
+import { CartModelState } from './cart';
 
 export interface ConnectProps {
-  dispatch: Dispatch;
   location: Location & { state: { from: string } };
+  dispatch: Dispatch;
 }
 
 export interface ConnectState {
   user: UserModelState;
+  cart: CartModelState;
 }
 
 export { UserModelState };
+export { CartModelState };
